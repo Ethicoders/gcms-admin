451319a920bd12a6df346916e79cd43fadf86e0d
Laegel <valentin@recital.ai>
2019-09-02 11:14:50 +0200

    Init

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Login extends Vue {}
+</script>
+
+<style lang="stylus">
+</style>
